6e54fa68a21b13a9ea30aaef3ab191d087ea1a01
Dave Font <gailees@umich.edu>
2021-04-10 02:52:35 +0800

    initialize ludobitclout

---
+++ LudoBitClout/LudoBitClout.h
@@ -0,0 +1,18 @@
+//
+//  LudoBitClout.h
+//  LudoBitClout
+//
+//  Created by hellyeah on 4/10/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LudoBitClout.
+FOUNDATION_EXPORT double LudoBitCloutVersionNumber;
+
+//! Project version string for LudoBitClout.
+FOUNDATION_EXPORT const unsigned char LudoBitCloutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LudoBitClout/PublicHeader.h>
+
+
